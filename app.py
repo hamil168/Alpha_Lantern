2a3bbf776808281010b02de6abe0a587ff324154
Joseph Curtin <jbcurtincode@gmail.com>
2018-09-10 10:26:44 -0700

    updated codes to work for demo

--- app.py
+++ app.py
@@ -2,12 +2,19 @@
 # Modified to load a model and to call the model prediction
 # and incorporate it into the Student() class attributes
 
-from flask import Flask, request, flash, url_for, redirect, render_template
+import pandas
+import json
+import uuid
+import random
+from flask import Flask, request, flash, url_for, redirect, render_template, Response
 from flask_sqlalchemy import SQLAlchemy
 from keras.models import load_model
 import keras
 from keras import backend as K
 import tensorflow as tf
+from scipy.stats import gmean
+
+from werkzeug.serving import run_simple
 
 #K.clear_session()
 
@@ -71,6 +78,58 @@ class students(db.Model):
 def show_all():
    return render_template('show_all.html', students = students.query.all() )
 
+@app.after_request
+def flask_request(response):
+  response.headers['Access-Control-Allow-Credentials'] = '*'
+  response.headers['Access-Control-Allow-Headers'] = '*'
+  response.headers['Access-Control-Allow-Origin'] = '*'
+  response.headers['Access-Control-Request-Headers'] = '*'
+  response.headers['Access-Control-Request-Methods'] = '*'
+  return response
+
+@app.route('/predict.json', methods=['POST'])
+def predict():
+  """
+  Test with Python, using a local REPL.
+  import json
+  import requests
+  from pprint import pprint
+
+  resp = requests.post(url, data=json.dumps({'behavior': ['jimmy hates school', 'he has a bully', 'avoids school']}), headers={'Content-Type': 'application/json'})
+  pprint(resp.json())
+  """
+  from model_utils import user_classification
+  results = []
+  with graph.as_default():
+    for phrase in request.json['behavior']:
+      yp, y_pred, y_class = user_classification(phrase, ml_model)
+      student = students(uuid.uuid4(), uuid.uuid4(),
+                  request.json['behavior'], random.randint(0, 9999),
+                  yp[0], y_pred[0][0], y_pred[0][1], y_pred[0][2], y_pred[0][3],
+                  y_class[0][0], y_class[0][1], y_class[0][2], y_class[0][3],
+                  CLASSIFICATION_THRESHOLD)
+
+      results.append([student.class1p, student.class2p, student.class3p, student.class4p])
+
+  df = pandas.DataFrame(results)
+  final_result = [gmean(df.iloc[:,col])/(4 / (df.shape[0])) for col in df.columns]
+  '''
+  <td>{{ student.name }}</td>
+  <td>{{ student.city }}</td>
+  <td>{{ student.behavior }}</td>
+  <td>{{ student.pin }}</td>
+  <td>{{ student.v_classes}}</td>
+  <td>{{ student.class1p}}</td>
+  <td>{{ student.class2p}}</td>
+  <td>{{ student.class3p}}</td>
+  <td>{{ student.class4p}}</td>
+  '''
+  return Response(json.dumps({
+    'behavior': request.json['behavior'],
+    'classification': final_result
+  }), 200, mimetype='application/json')
+
+
 # Get info POST'd from the front end, check its validit
 # If valid, populate the object 'student' with the entered info
 @app.route('/new', methods = ['GET', 'POST'])
@@ -107,11 +166,12 @@ def new():
 
 
 if __name__ == '__main__':
+  
+  # Creates the little database and executes with debugging on
+  db.create_all()
 
-   # Creates the little database and executes with debugging on
-   db.create_all()
+  with graph.as_default():
+    ml_model = load_model('./ml_model/model.h5')
 
-   with graph.as_default():
-       ml_model = load_model('./ml_model/model.h5')
-
-   app.run(debug = True)
+  # http://flask.pocoo.org/docs/1.0/patterns/appdispatch/
+  run_simple('127.0.0.1', 5000, app, use_reloader=False, use_debugger=True)
